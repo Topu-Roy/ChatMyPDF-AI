22f66c98a6095f20de0588ed716a9054cbb7859c
Topu-Roy <topuroy143333@gmail.com>
2023-09-29 01:30:53 +0600

    max width container

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,10 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+export default function MaxWidthWrapper({ children, className }: { children: React.ReactNode, className?: string }) {
+    return (
+        <div className={cn("mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className)}>
+            {children}
+        </div>
+    )
+}
