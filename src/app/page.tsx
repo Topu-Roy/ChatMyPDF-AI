ed87b24bb8aa14f45815a4ec6524f962051178f0
Topu-Roy <topuroy143333@gmail.com>
2023-09-29 03:12:23 +0600

    Second gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,10 +37,12 @@ export default function Home() {
         </Link>
       </MaxWidthWrapper>
 
-      {/* Decoration */}
-      <div area-hidden="true">
+      {/* Product Image */}
+      <div>
         <div className="relative isolate">
-          <div className="pointer-events-none absolute inset-x-0 -top-40 -z-10 overflow-hidden transform-gpu blur-3xl sm:-top-80">
+
+          {/* Background Gradient */}
+          <div area-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 overflow-hidden transform-gpu blur-3xl sm:-top-80">
             <div
               style={{
                 clipPath:
@@ -66,6 +68,18 @@ export default function Home() {
               </div>
             </div>
           </div>
+
+          {/* Background Gradient */}
+          <div area-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 overflow-hidden transform-gpu blur-3xl sm:-top-80">
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(95%-1rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[90deg] bg-gradient-to-tr from-[#80ffc2] to-[#81e673] opacity-30 sm:left-[calc(95%-5rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+
         </div>
       </div>
     </>
