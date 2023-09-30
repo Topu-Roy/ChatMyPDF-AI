dd4912e84325440c2e0cda7efa46191b6d1d6b50
Topu-Roy <topuroy143333@gmail.com>
2023-09-30 19:15:17 +0600

    Creating the Dashboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,26 @@
+import DashboardComponent from '@/components/DashboardComponent'
+import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
-import React from 'react'
 
-export default function Dashboard() {
+export default async function Dashboard() {
     const { getUser } = getKindeServerSession()
     const user = getUser()
 
+    // * If not logged in
     if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
+    // * Check if user is synced to DB
+    const dbUser = await db.user.findFirst({
+        where: {
+            kindeId: user.id,
+        }
+    })
+
+    // * If not found on redirect for syncing
+    if (!dbUser) redirect('/auth-callback?origin=dashboard')
+
     return (
-        <div>{user.email}</div>
+        <DashboardComponent />
     )
 }
