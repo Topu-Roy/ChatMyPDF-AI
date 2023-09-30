daaac381a664967e2044eedf5baf9a40b279f567
Topu-Roy <topuroy143333@gmail.com>
2023-09-30 16:10:18 +0600

    Sync user with the DB

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,15 +1,33 @@
-import { useRouter, useSearchParams } from 'next/navigation'
+'use client'
+
+import { redirect, useSearchParams } from 'next/navigation'
 import { trpc } from '../_trpc/client'
+import { Loader2 } from 'lucide-react'
 
 export default function AuthCallback() {
-    const router = useRouter()
     const searchParams = useSearchParams()
-
     const origin = searchParams.get('origin')
 
-    const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    trpc.authCallback.useQuery(undefined, {
         onSuccess: ({ success }) => {
-            if (success) router.push(origin ? `${origin}` : '/dashboard')
-        }
+            if (success) redirect(origin ? `${origin}` : '/dashboard')
+        },
+        onError: ({ data }) => {
+            if (data?.code === "UNAUTHORIZED") {
+                redirect('/sign-in')
+            }
+        },
+        retry: true,
+        retryDelay: 500
     })
+
+    return (
+        <div className="w-full mt-24 flex justify-center">
+            <div className="flex flex-col items-center gap-2">
+                <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+                <h3 className='font-semibold text-xl'>Setting up your account...</h3>
+                <p className=''>You will be redirected automatically.</p>
+            </div>
+        </div>
+    )
 }
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,17 +1,34 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { router, publicProcedure } from "./trpc";
 import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = getUser();
 
-    if (!user || !user.email) {
+    if (!user || !user.email || !user.id) {
       throw new TRPCError({ code: "UNAUTHORIZED" });
     }
 
     // TODO : Check if the user is in the DB
+    const dbUser = await db.user.findFirst({
+      where: {
+        kindeId: user.id?.toString(),
+      },
+    });
+
+    if (!dbUser) {
+      // Create a new user in database
+      await db.user.create({
+        data: {
+          name: `${user.given_name} ${user.family_name}`,
+          kindeId: user.id?.toString(),
+          email: user.email,
+        },
+      });
+    }
 
     return { success: true };
   }),
