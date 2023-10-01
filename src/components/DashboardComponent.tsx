196ea05a6082e7094eeedcd4e0ae6f9021f6dc76
Topu-Roy <topuroy143333@gmail.com>
2023-10-02 01:57:30 +0600

    Added Date Fns & Made the file card

--- src/components/DashboardComponent.tsx
+++ src/components/DashboardComponent.tsx
@@ -3,11 +3,20 @@
 import React from "react";
 import UploadButton from "./UploadButton";
 import { trpc } from "@/app/_trpc/client";
-import { Ghost } from "lucide-react";
+import { Ghost, MessageSquare, Plus, Trash } from "lucide-react";
 import { Skeleton } from "./ui/skeleton";
+import Link from "next/link";
+import { format } from "date-fns";
+import { Button } from "./ui/button";
 
 export default function DashboardComponent() {
     const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+
+    const sortedFiles = files?.sort(
+        (a, b) =>
+            new Date(b.createdAt).getTime() -
+            new Date(a.createdAt).getTime()
+    )
 
     return (
         <main className="mx-auto max-w-7xl px-2 md:p-10">
@@ -19,7 +28,39 @@ export default function DashboardComponent() {
 
             {/* Show user files */}
             {files && files.length !== 0 ? (
-                <div>files</div>
+                <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
+                    {sortedFiles?.map((file) => (
+                        <li key={file.id} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white transition hover:shadow-lg">
+                            <Link
+                                href={`/dashboard/${file.kindeUserId}`}
+                                className="flex flex-col gap-2"
+                            >
+                                <div className="pt-6 px-6 flex w-full justify-between items-center space-x-6">
+                                    <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+                                    <div className="flex-1 truncate">
+                                        {file.name}
+                                    </div>
+                                </div>
+                            </Link>
+
+                            <div className='px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500'>
+                                <div className="flex items-center gap-2">
+                                    <Plus className="h-4 w-4" />
+                                    {format(new Date(file.createdAt), "dd MMM, yyyy")}
+                                </div>
+
+                                <div className="flex items-center gap-2">
+                                    <MessageSquare className="h-4 w-4" />
+                                    <p>Mocked</p>
+                                </div>
+
+                                <Button size={'sm'} variant={'destructive'} className="w-full">
+                                    <Trash className="h-4 w-4" />
+                                </Button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             ) : isLoading ? (
                 <div className="flex items-center justify-center gap-4 flex-col w-full">
                     <Skeleton className="h-[10vh] w-[100%] bg-slate-400/20" />
