058bb6810fddfac0984fadae78cd56331d7a4dc3
Topu-Roy <topuroy143333@gmail.com>
2023-10-02 17:11:29 +0600

    React-PDF to render PDF files

--- src/components/PDFRenderer.tsx
+++ src/components/PDFRenderer.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function PDFRenderer() {
     return (
-        <div>PDFRenderer</div>
+        <div className='w-full bg-white rounded-md shadow flex items-center flex-col'>
+            <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+                <div className="flex items-center gap-1.5">
+                    Top Bar
+                </div>
+            </div>
+        </div>
     )
 }
