acde081eefeede05d9f03eb4a0b5045d9cf473e7
Topu-Roy <topuroy143333@gmail.com>
2023-10-03 16:28:07 +0600

    React-Dropzone for Drag and drop files for upload

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
 import { Button } from "./ui/button"
+import UploadDropzone from "./UploadDropzone"
 
 export default function UploadButton() {
     const [isOpen, setIsOpen] = useState(false)
@@ -19,7 +20,7 @@ export default function UploadButton() {
             </DialogTrigger>
 
             <DialogContent>
-                dialog content
+                <UploadDropzone />
             </DialogContent>
 
         </Dialog>
---
+++ src/components/UploadDropzone.tsx
@@ -0,0 +1,48 @@
+import { Cloud, File } from "lucide-react";
+import React from "react";
+import Dropzone from "react-dropzone";
+
+export default function UploadDropzone() {
+    return (
+        <Dropzone
+            multiple={false}
+            onDrop={(acceptedFiles) => console.log(acceptedFiles)}
+        >
+            {({ getRootProps, getInputProps, acceptedFiles }) => (
+                <div
+                    {...getRootProps()}
+                    className="border-[2px] h-64 m-4 border-dashed border-gray-300 rounded-lg"
+                >
+                    <div className="flex justify-center items-center h-full w-full">
+                        <label
+                            htmlFor="dropzone_file"
+                            className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-100 hover:bg-gray-200"
+                        >
+                            <div className="flex flex-col justify-center items-center pt-5 pb-6">
+                                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                                <p className="mb-2 text-sm text-zinc-700">
+                                    <span className="font-semibold">Click to upload</span> or Drag 'n' drop PDF
+                                </p>
+                            </div>
+
+                            {acceptedFiles && acceptedFiles[0] ? (
+                                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline-[1.5px] outline-zinc-200 divide-x divide-zinc-200">
+                                    <div className="px-3 py-2 grid place-items-center">
+                                        <File className="h-4 w-4 text-blue-500" />
+                                    </div>
+                                    <div className="px-3 py-2 truncate">
+                                        <p className=" text-sm truncate">
+                                            {acceptedFiles[0].name}
+                                        </p>
+                                    </div>
+                                </div>
+                            ) : null}
+
+                        </label>
+                        <input id="dropzone_file" {...getInputProps()} />
+                    </div>
+                </div>
+            )}
+        </Dropzone>
+    );
+}
