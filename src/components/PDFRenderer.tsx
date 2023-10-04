66736f2d9d152391b9556612eea30f2c7148c6a4
Topu-Roy <topuroy143333@gmail.com>
2023-10-05 02:15:08 +0600

    Now can control and navigate to different pages of the pdf

--- src/components/PDFRenderer.tsx
+++ src/components/PDFRenderer.tsx
@@ -1,13 +1,16 @@
 'use client'
 
-import { Loader2 } from 'lucide-react';
-import { useToast } from './ui/use-toast';
-import { useResizeDetector } from 'react-resize-detector';
-
 //* React PDF
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
+
+import { useResizeDetector } from 'react-resize-detector';
+import { ChevronDown, ChevronUp, Loader2 } from 'lucide-react';
+import { useToast } from './ui/use-toast';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { useState } from 'react';
 
 //* PDF worker
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
@@ -17,16 +20,49 @@ type PDFRendererProps = {
 }
 
 export default function PDFRenderer({ url }: PDFRendererProps) {
+    const [numberOfPagesInPDF, setNumberOfPagesInPDF] = useState<number>()
+    const [currentPageOfPDF, setCurrentPageOfPDF] = useState<number>(1)
 
     const { toast } = useToast()
-
     const { width, ref } = useResizeDetector()
 
     return (
         <div className='w-full bg-white rounded-md shadow flex items-center flex-col'>
             <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
                 <div className="flex items-center gap-1.5">
-                    Top Bar
+                    <Button
+                        disabled={currentPageOfPDF <= 0}
+                        onClick={() => (
+                            setCurrentPageOfPDF(prev => (
+                                prev === 1 ? 1 : prev - 1
+                            ))
+                        )}
+                        variant={'ghost'}
+                        aria-label='Previous page'
+                    >
+                        <ChevronDown className='h-4 w-4' />
+                    </Button>
+
+                    <div className="flex items-center gap-1.5">
+                        <Input className='w-12 h-8' />
+                        <p className='text-zinc-700 text-sm space-x-1'>
+                            <span>/</span>
+                            <span>{numberOfPagesInPDF ?? 'x'}</span>
+                        </p>
+                    </div>
+
+                    <Button
+                        disabled={numberOfPagesInPDF === undefined || currentPageOfPDF === numberOfPagesInPDF}
+                        onClick={() => (
+                            setCurrentPageOfPDF(prev => (
+                                prev === numberOfPagesInPDF ? numberOfPagesInPDF : prev + 1
+                            ))
+                        )}
+                        variant={'ghost'}
+                        aria-label='Previous page'
+                    >
+                        <ChevronUp className='h-4 w-4' />
+                    </Button>
                 </div>
             </div>
 
@@ -40,15 +76,15 @@ export default function PDFRenderer({ url }: PDFRendererProps) {
                                 <Loader2 className='h-6 w-6 my-24 animate-spin text-zinc-800' />
                             </div>
                         }
+                        onLoadSuccess={({ numPages }) => setNumberOfPagesInPDF(numPages)}
                         onError={() => (
                             toast({
                                 title: "Couldn't get the file",
                                 description: "Something went wrong, please try again"
                             }))
-
                         }
                     >
-                        <Page width={width ? width : 1} pageNumber={1} />
+                        <Page width={width ? width : 1} pageNumber={currentPageOfPDF} />
                     </Document>
                 </div>
             </div>
