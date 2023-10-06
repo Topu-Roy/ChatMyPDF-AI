a3ebbdf74879b3d23e1a406235b7e99d66482c1c
Topu-Roy <topuroy143333@gmail.com>
2023-10-07 00:55:57 +0600

    Wraped with the context

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -7,6 +7,7 @@ import { ChevronLeft, Loader2, XCircle } from 'lucide-react'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '../ui/button'
+import { ChatContext, ChatContextProvider } from './ChatContext'
 
 export default function ChatWrapper({ fileId }: { fileId: string }) {
 
@@ -83,12 +84,14 @@ export default function ChatWrapper({ fileId }: { fileId: string }) {
     }
 
     return (
-        <div className='relative min-h-full bg-zinc-50 divide-y divide-zinc-200 flex flex-col justify-between gap-2'>
-            <div className="flex-1 justify-between flex flex-col mb-28">
-                <Messages />
-            </div>
+        <ChatContextProvider fileId={fileId}>
+            <div className='relative min-h-full bg-zinc-50 divide-y divide-zinc-200 flex flex-col justify-between gap-2'>
+                <div className="flex-1 justify-between flex flex-col mb-28">
+                    <Messages />
+                </div>
 
-            <ChatInput />
-        </div>
+                <ChatInput />
+            </div>
+        </ChatContextProvider>
     )
 }
