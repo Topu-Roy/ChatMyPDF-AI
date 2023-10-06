5cd173b906ab2c933a2cf9d70c6bb6648d16ad42
Topu-Roy <topuroy143333@gmail.com>
2023-10-07 00:32:08 +0600

    Added the send button

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,10 +1,10 @@
+'use client'
+
+import { SendIcon } from 'lucide-react'
+import { Button } from '../ui/button'
 import { Textarea } from '../ui/textarea'
 
-type Props = {
-    isDisabled?: boolean
-}
-
-export default function ChatInput({ isDisabled }: Props) {
+export default function ChatInput({ isDisabled }: { isDisabled?: boolean }) {
     return (
         <div className='absolute bottom-0 left-0 w-full'>
             <form className='mx-2 flex flex-row gap-3 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl'>
@@ -18,6 +18,10 @@ export default function ChatInput({ isDisabled }: Props) {
                                 placeholder='Enter your prompt text'
                                 className='resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'
                             />
+
+                            <Button className='absolute bottom-1.5 right-[8px]' aria-label='Send Message'>
+                                <SendIcon className='h-4 w-4' />
+                            </Button>
                         </div>
                     </div>
                 </div>
