ee1b41b09a0378b2f1e9fa7aa691b0ef72d99a47
Topu-Roy <topuroy143333@gmail.com>
2023-10-08 00:25:28 +0600

    Send buttton working

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,10 +1,14 @@
-'use client'
-
 import { SendIcon } from 'lucide-react'
 import { Button } from '../ui/button'
 import { Textarea } from '../ui/textarea'
+import { useContext, useRef } from 'react'
+import { ChatContext } from './ChatContext'
 
 export default function ChatInput({ isDisabled }: { isDisabled?: boolean }) {
+
+    const { addMessage, handleInputChange, isLoading, message } = useContext(ChatContext)
+    const textAreaRef = useRef<HTMLTextAreaElement | null>(null)
+
     return (
         <div className='absolute bottom-0 left-0 w-full'>
             <form className='mx-2 flex flex-row gap-3 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl'>
@@ -17,9 +21,30 @@ export default function ChatInput({ isDisabled }: { isDisabled?: boolean }) {
                                 autoFocus
                                 placeholder='Enter your prompt text'
                                 className='resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'
+                                onChange={handleInputChange}
+                                value={message}
+                                ref={textAreaRef}
+                                onKeyDown={(e) => {
+                                    if (e.key === 'Enter' && !e.shiftKey) {
+                                        e.preventDefault();
+
+                                        addMessage()
+                                        textAreaRef.current?.focus();
+                                    }
+                                }}
                             />
 
-                            <Button className='absolute bottom-1.5 right-[8px]' aria-label='Send Message'>
+                            <Button
+                                disabled={isLoading || isDisabled || message === ''}
+                                className='absolute bottom-1.5 right-[8px]'
+                                aria-label='Send Message'
+                                type='submit'
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    addMessage()
+                                    textAreaRef.current?.focus();
+                                }}
+                            >
                                 <SendIcon className='h-4 w-4' />
                             </Button>
                         </div>
