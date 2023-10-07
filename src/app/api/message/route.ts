b9336e5ade5eb59141df87875bce107c50fb74d4
Topu-Roy <topuroy143333@gmail.com>
2023-10-07 19:35:10 +0600

    Gave it a prompt

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -73,6 +73,31 @@ export async function POST(req: NextRequest) {
     model: "gpt-3.5-turbo",
     temperature: 0,
     stream: true,
-    messages: [],
+    messages: [
+      {
+        role: "system",
+        content:
+          "Use the context below or previous conversation (if needed) to answer the users question in markdown format.",
+      },
+      {
+        role: "user",
+        content: `Use the context below or previous conversation (if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+        
+    \n----------------\n
+    
+    PREVIOUS CONVERSATION:
+    ${formattedPreviousMessages.map((message) => {
+      if (message.role === "user") return `User: ${message.content}\n`;
+      return `Assistant: ${message.content}\n`;
+    })}
+    
+    \n----------------\n
+    
+    CONTEXT:
+    ${result.map((r) => r.pageContent).join("\n\n")}
+    
+    USER INPUT: ${message}`,
+      },
+    ],
   });
 }
