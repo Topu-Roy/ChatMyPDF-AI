b7bf6749375136662a252eb135ed9136cc68adf9
Topu-Roy <topuroy143333@gmail.com>
2023-10-09 01:33:09 +0600

    Trpc query form the front-end

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,8 +1,22 @@
+import { trpc } from '@/app/_trpc/client'
+import { INFINITE_QUERY_DEFAULT_LIMIT } from '@/lib/constConfig/infinite-query'
 import React from 'react'
 
-type Props = {}
+type Props = {
+    fileId: string
+}
 
-function Messages({ }: Props) {
+function Messages({ fileId }: Props) {
+
+    const { data, isLoading } = trpc.getFileMessages.useInfiniteQuery({
+        fileId,
+        limit: INFINITE_QUERY_DEFAULT_LIMIT
+    }, {
+        getNextPageParam: ({ nextCursor }) => nextCursor,
+        keepPreviousData: true
+    }
+    )
+
     return (
         <div>Messages</div>
     )
