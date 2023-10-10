0c0683c1942ffa9e117d93519156bc90d12f4330
Topu-Roy <topuroy143333@gmail.com>
2023-10-11 00:50:00 +0600

    Added the icons beside the messages

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils'
 import { MessageType } from '@/types/message'
 import React from 'react'
+import { Icons } from '../MyIcons'
 
 type Props = {
     message: MessageType,
@@ -12,11 +13,21 @@ function Message({ message, isNextMessageFromSamePerson }: Props) {
         <div className={cn('flex items-end', {
             'justify-end': message.isUserMessage
         })}>
-            <div className={cn('relative flex h-6 w-6 aspect-square items-center justify-center rounded-sm', {
-                'order-2 bg-blue-600': message.isUserMessage,
-                'order-1 bg-zinc-800': !message.isUserMessage,
-                invisible: isNextMessageFromSamePerson
-            })}></div>
+            <div
+                className={cn('relative flex h-6 w-6 aspect-square items-center justify-center rounded-sm', {
+                    'order-2 bg-blue-600': message.isUserMessage,
+                    'order-1 bg-zinc-800': !message.isUserMessage,
+                    invisible: isNextMessageFromSamePerson
+                })}
+            >
+                {
+                    message.isUserMessage ? (
+                        <Icons.user className='fill-zinc-200 text-zinc-200 h-3/4 w-3/4' />
+                    ) : (
+                        <Icons.logo className='fill-zinc-200 text-zinc-200 h-3/4 w-3/4' />
+                    )
+                }
+            </div>
         </div>
     )
 }
