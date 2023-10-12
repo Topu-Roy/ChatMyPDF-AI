69d661c2ec4076fc8d125dceafdf6647718a4137
Topu-Roy <topuroy143333@gmail.com>
2023-10-12 22:32:28 +0600

    Added tooltip from shadcn-ui

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -12,7 +12,15 @@ function PricingPage({ }: Props) {
     return (
         <>
             <MaxWidthWrapper className='mb-8 mt-24 text-center max-w-5xl'>
-                <div className=""></div>
+                <div className="mx-auto mb10\ sm:max-w-lg">
+                    <h1 className='text-6xl font-bold sm:text-7xl'>
+                        Pricing
+                    </h1>
+                    <p className="mt-5 text-gray-600 sm:text-lg">
+                        Whether you&apos;re just trying out our service or
+                        need more, we&apos;ve got you covered.
+                    </p>
+                </div>
             </MaxWidthWrapper>
         </>
     )
