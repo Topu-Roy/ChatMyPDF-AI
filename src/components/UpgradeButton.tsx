e19e1fa08959be7049a6bf0d6bbe0381df059147
Topu-Roy <topuroy143333@gmail.com>
2023-10-13 00:29:19 +0600

    Api call from the client

--- src/components/UpgradeButton.tsx
+++ src/components/UpgradeButton.tsx
@@ -1,12 +1,20 @@
+'use client'
+
 import React from 'react'
 import { Button } from './ui/button'
 import { ArrowRight } from 'lucide-react'
+import { trpc } from '@/app/_trpc/client'
 
-type Props = {}
+function UpgradeButton() {
 
-function UpgradeButton({ }: Props) {
+    const { mutate: createStripeSession } = trpc.createStripeSession.useMutation({
+        onSuccess: ({ url }) => {
+            window.location.href = url ?? '/dashboard/billing/'
+        }
+    })
+
     return (
-        <Button className='w-full bg-blue-600'>
+        <Button onClick={() => createStripeSession()} className='w-full bg-blue-600'>
             Upgrade now
             <ArrowRight className='h-5 w-5 ml-1.5' />
         </Button>
