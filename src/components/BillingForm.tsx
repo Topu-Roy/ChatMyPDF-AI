b712f3b346cc121812f2c5c95363728e8d32e0a7
Topu-Roy <topuroy143333@gmail.com>
2023-10-13 14:25:55 +0600

    TRPC call

--- src/components/BillingForm.tsx
+++ src/components/BillingForm.tsx
@@ -1,10 +1,39 @@
-import { getUserSubscriptionPlan } from '@/lib/stripe'
+'use client'
+
 import { SubscriptionPlanType } from '@/types/subscription-form';
 import React from 'react'
+import { useToast } from './ui/use-toast';
+import { trpc } from '@/app/_trpc/client';
+import MaxWidthWrapper from './MaxWidthWrapper';
 
-function BillingForm({ }: SubscriptionPlanType) {
+function BillingForm({ subscriptionPlan }: SubscriptionPlanType) {
+    const { toast } = useToast()
+
+    const { mutate: createStripeSession, isLoading } = trpc.createStripeSession.useMutation({
+        onSuccess: ({ url }) => {
+            if (url) {
+                window.location.href = url
+            } else {
+                toast({
+                    title: "Something went wrong...",
+                    description: "Please try again in a moment",
+                    variant: "destructive"
+                })
+            }
+        }
+    })
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        createStripeSession()
+    }
+
     return (
-        <div>BillingForm</div>
+        <MaxWidthWrapper className='max-w-5xl'>
+            <form className='mt-12' onSubmit={handleSubmit}>
+
+            </form>
+        </MaxWidthWrapper>
     )
 }
 
