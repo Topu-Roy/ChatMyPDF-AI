9d08f30b3e7ba8a93f23b1158de13226f0a759b4
Topu-Roy <topuroy143333@gmail.com>
2023-10-15 21:58:21 +0600

    Logic for plans when uploading files

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -10,103 +10,118 @@ import { PLANS } from "@/lib/constConfig/stripe";
 
 const f = createUploadthing();
 
-export const ourFileRouter = {
-  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
-    .middleware(async () => {
-      //* Authentication checking
-      const { getUser } = getKindeServerSession();
-      const user = getUser();
+const middleware = async () => {
+  //* Authentication checking
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
 
-      if (!user || !user.id) throw new Error("Unauthorized");
+  if (!user || !user.id) throw new Error("Unauthorized");
 
-      //* Subscriptions checking
-      const subscription = await getUserSubscriptionPlan();
+  //* Subscriptions checking
+  const subscription = await getUserSubscriptionPlan();
 
-      return { subscription, userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => {
-      //* add the file info to the database
+  return { subscription, userId: user.id };
+};
 
-      const createdFile = await db.file.create({
+type onUploadCompleteType = {
+  metadata: Awaited<ReturnType<typeof middleware>>;
+  file: {
+    name: string;
+    key: string;
+  };
+};
+
+const onUploadComplete = async ({ metadata, file }: onUploadCompleteType) => {
+  //* add the file info to the database
+
+  const createdFile = await db.file.create({
+    data: {
+      name: file.name,
+      kindeId: metadata.userId,
+      uploadStatus: "PROCESSING",
+      url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+      key: file.key,
+    },
+  });
+
+  //* Index the file into vector databases
+  try {
+    const res = await fetch(
+      `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+    );
+    //* Make blob object
+    const blob = await res.blob();
+
+    //* Load the file in memory
+    const loader = new PDFLoader(blob);
+
+    const pdfPageData = await loader.load();
+    const amountOfPages = pdfPageData.length;
+
+    //* Subscription based features
+    const { isSubscribed } = metadata.subscription;
+
+    const isProPlanExceededLimit =
+      amountOfPages > PLANS.find((p) => p.name === "Pro")!.pagesPerPdf;
+    const isFreePlanExceededLimit =
+      amountOfPages > PLANS.find((p) => p.name === "Free")!.pagesPerPdf;
+
+    if (
+      (isSubscribed && isProPlanExceededLimit) ||
+      (!isSubscribed && isFreePlanExceededLimit)
+    ) {
+      await db.file.update({
         data: {
-          name: file.name,
-          kindeId: metadata.userId,
-          uploadStatus: "PROCESSING",
-          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-          key: file.key,
+          uploadStatus: "FAILED",
+        },
+        where: {
+          id: createdFile.id,
         },
       });
+    }
 
-      //* Index the file into vector databases
-      try {
-        const res = await fetch(
-          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
-        );
-        //* Make blob object
-        const blob = await res.blob();
+    //* Vectorized and index the pdf in PineconeDB
+    const pineconeIndex = pinecone.Index("chatmypdf");
 
-        //* Load the file in memory
-        const loader = new PDFLoader(blob);
+    //* Make embedding of the pdf with langchain
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPEN_AI_API_KEY,
+    });
 
-        const pdfPageData = await loader.load();
-        const amountOfPages = pdfPageData.length;
+    //* Insert the embedding into pinecone
+    await PineconeStore.fromDocuments(pdfPageData, embeddings, {
+      pineconeIndex,
+      namespace: createdFile.id,
+    });
 
-        //* Subscription based features
-        const { isSubscribed } = metadata.subscription;
+    //* after everything is done
+    await db.file.update({
+      data: {
+        uploadStatus: "SUCCESS",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  } catch (error) {
+    await db.file.update({
+      data: {
+        uploadStatus: "FAILED",
+      },
+      where: {
+        id: createdFile.id,
+      },
+    });
+  }
+};
 
-        const isProPlanExceededLimit =
-          amountOfPages > PLANS.find((p) => p.name === "Pro")!.pagesPerPdf;
-        const isFreePlanExceededLimit =
-          amountOfPages > PLANS.find((p) => p.name === "Free")!.pagesPerPdf;
-
-        if (
-          (isSubscribed && isProPlanExceededLimit) ||
-          (!isSubscribed && isFreePlanExceededLimit)
-        ) {
-          await db.file.update({
-            data: {
-              uploadStatus: "FAILED",
-            },
-            where: {
-              id: createdFile.id,
-            },
-          });
-        }
-
-        //* Vectorized and index the pdf in PineconeDB
-        const pineconeIndex = pinecone.Index("chatmypdf");
-
-        //* Make embedding of the pdf with langchain
-        const embeddings = new OpenAIEmbeddings({
-          openAIApiKey: process.env.OPEN_AI_API_KEY,
-        });
-
-        //* Insert the embedding into pinecone
-        await PineconeStore.fromDocuments(pdfPageData, embeddings, {
-          pineconeIndex,
-          namespace: createdFile.id,
-        });
-
-        //* after everything is done
-        await db.file.update({
-          data: {
-            uploadStatus: "SUCCESS",
-          },
-          where: {
-            id: createdFile.id,
-          },
-        });
-      } catch (error) {
-        await db.file.update({
-          data: {
-            uploadStatus: "FAILED",
-          },
-          where: {
-            id: createdFile.id,
-          },
-        });
-      }
-    }),
+export const ourFileRouter = {
+  freePlanUploader: f({ pdf: { maxFileSize: "4MB" } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
+  proPlanUploader: f({ pdf: { maxFileSize: "16MB" } })
+    .middleware(middleware)
+    .onUploadComplete(onUploadComplete),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
